daddd0ab87ea2ab1d74434443a463930f82a7944
ctrl-alt-delli <johnsondelmikao.j@gmail.com>
2025-02-26 22:25:42 -0500

    Update hardware_wamos.ino

--- hardware_wamos/hardware_wamos.ino
+++ hardware_wamos/hardware_wamos.ino
@@ -1,167 +1,189 @@
 
+#ifndef STDLIB_H
+#include <stdlib.h>
+#endif
+
+#ifndef STDIO_H
+#include <stdio.h>
+#endif
+
+#ifndef ARDUINO_H
+#include <Arduino.h>
+#endif 
+ 
+#ifndef ARDUINOJSON_H
+#include <ArduinoJson.h>
+#endif
+
+// DEFINE VARIABLES
+#define ARDUINOJSON_USE_DOUBLE      1 
 
 #include <SoftwareSerial.h>
-#include <ArduinoJson.h>
-
-#include <NewPing.h>
+// IMPORT ALL REQUIRED LIBRARIES
 
 #include <math.h>
-
+   
 //**********ENTER IP ADDRESS OF SERVER******************//
 
-#define HOST_IP "www.yanacreations.com" // REPLACE with IP address of your server
-#define HOST_PORT "8080"         // REPLACE with your server port
-#define route "api/update"       // leave unchanged
-#define idNumber "620162297"     // REPLACE with your ID number
+#define HOST_IP     "104.244.227.77"       // REPLACE WITH IP ADDRESS OF SERVER ( IP ADDRESS OF COMPUTER THE BACKEND IS RUNNING ON) 
+#define HOST_PORT   "8080"            // REPLACE WITH SERVER PORT (BACKEND FLASK API PORT)
+#define route       "api/update"      // LEAVE UNCHANGED 
+#define idNumber    "620157584"       // REPLACE WITH YOUR ID NUMBER 
 
 // WIFI CREDENTIALS
-#define SSID "MonaConnect" // REPLACE with your WiFi SSID
-#define password ""      // REPLACE with your WiFi password
+//#define SSID        "MonaConnect"      // "REPLACE WITH YOUR WIFI's SSID"   
+//#define password    ""  // "REPLACE WITH YOUR WiFi's PASSWORD" 
+#define SSID        "Digicel_WiFi_eSEb"      // "REPLACE WITH YOUR WIFI's SSID"   
+#define password    "WYPx3kCk"  // "REPLACE WITH YOUR WiFi's PASSWORD" 
 
-#define stay 100
-
+#define stay        100
+ 
 //**********PIN DEFINITIONS******************//
 
-#define espRX 10
-#define espTX 11
+ 
+#define espRX         10
+#define espTX         11
 #define espTimeout_ms 300
+#define TRIG 5
+#define ECHO 6
 
-// HC‐SR04 sensor pins (using pins 12 and 13 since 10 & 11 go to the ESP)
-#define TRIGGER_PIN 12   // Arduino pin tied to trigger pin on the ultrasonic sensor.
-#define ECHO_PIN 13      // Arduino pin tied to echo pin on the ultrasonic sensor.
-#define MAX_DISTANCE 500 // Maximum distance we want to ping for in inches
-
+ 
+ 
 /* Declare your functions below */
-void espSend(char command[]);
+void espSend(char command[] );
 void espUpdate(char mssg[]);
-void espInit();
-double getWaterHeight(double radar);
-double getRadar();
-double getWaterReserve(double height);
+float checkWaterheight();
+float checkReserve();
+float detpercentage();
 
-SoftwareSerial esp(espRX, espTX);
-NewPing sonar(TRIGGER_PIN, ECHO_PIN, MAX_DISTANCE); // NewPing setup of pins and maximum distance.
+float percentage, reserve, duration, distance, sensorheight = 94.5, maxheight = 77.763, radius = 30.75;
 
-//**********TANK DIMENSIONS & SENSOR HEIGHT**********//
-#define MAX_WATER_HEIGHT 77.763 // Maximum water level/height from the base of the tank
-#define MAX_CAPACITY 1000       // Maximum capacity of the tank in US Gallons
-#define SENSOR_HEIGHT 94.5      // Ultrasonic sensor height from the base of the tank
-#define TANK_DIAMETER 61.5      // Diameter of the tank
-#define TANK_RADIUS TANK_DIAMETER / 2
 
-void setup()
-{
+SoftwareSerial esp(espRX, espTX); 
+ 
 
-  Serial.begin(115200);
+void setup(){
+
+  Serial.begin(115200); 
   // Configure GPIO pins here
+  pinMode(TRIG, OUTPUT);
+  pinMode(ECHO, INPUT);
 
-  espInit();
+  espInit();  
 }
 
-void loop()
-{
+void loop(){ 
 
+  checkWaterheight();
+  checkReserve();
+  detpercentage();
+
+  Serial.print(F("Radar: "));
+  Serial.print(distance);
+  Serial.print(F("inches\n Water Height: "));
+  Serial.print(checkWaterheight());
+  Serial.print(F("inches\n "));
+  Serial.print(F("Reserve: "));
+  Serial.print(checkReserve());
+  Serial.print(F("\n Percentage: "));
+  Serial.print(detpercentage());
+  
+  JsonDocument doc; // Create JSon object
+    char message[800]  = {0};
+
+    // Add key:value pairs to Json object according to below schema
+    doc["id"]         = "620157584"; // Change to your student ID number
+    doc["type"]  = "ultrasonic";
+    doc["radar"]  = distance;
+    doc["waterheight"]  = checkWaterheight();
+    doc["reserve"]  = checkReserve();
+    doc["percentage"]  = detpercentage();
+    /*Add code here to insert all other variabes that are missing from Json object
+    according to schema above
+    */
+
+    serializeJson(doc, message);  // Seralize / Covert JSon object to JSon string and store in char* array  
+    espUpdate(message);
+   
   // send updates with schema ‘{"id": "student_id", "type": "ultrasonic", "radar": 0, "waterheight": 0, "reserve": 0, "percentage": 0}’
-  // Continuously measure the distance between the sensor and the water in the tank. This must be considered as the ‘radar’ measurement.
-
-  // send updates with schema ‘{"id": "student_id", "type": "ultrasonic", "radar": 0, "waterheight": 0, "reserve": 0, "percentage": 0}’
-  double radar = sonar.ping_in();
-  double WaterHeight = getWaterHeight(radar);
-
-  double reserve = getWaterReserve(WaterHeight);        // Calculate water reserve
-  double percentage = (WaterHeight / MAX_WATER_HEIGHT) * 100; // Calculate percentage of water in the tank
-
-  StaticJsonDocument<290> doc;
-  char mssg[290] = {0};
-
-  Serial.println(radar);
-
-  doc["id"] = "620162297";
-  doc["type"] = "ultrasonic";
-  doc["radar"] = radar;
-  doc["waterheight"] = WaterHeight;
-  doc["reserve"] = reserve;
-  doc["percentage"] = percentage;
-
-  serializeJson(doc, msg);
-  espUpdate(msg);
-
-  delay(1000);
+  delay(1000);  
 }
 
-void espSend(char command[])
-{
-  esp.print(command); // send the read character to the esp
-  while (esp.available())
-  {
-    Serial.println(esp.readString());
-  }
+ 
+void espSend(char command[] ){   
+    esp.print(command); // send the read character to the esp    
+    while(esp.available()){ Serial.println(esp.readString());}    
 }
 
-void espUpdate(char mssg[])
-{
-  char espCommandString[50] = {0};
-  char post[290] = {0};
 
-  snprintf(espCommandString, sizeof(espCommandString), "AT+CIPSTART=\"TCP\",\"%s\",%s\r\n", HOST_IP, HOST_PORT);
-  espSend(espCommandString); // starts the connection to the server
-  delay(stay);
+void espUpdate(char mssg[]){ 
+    char espCommandString[50] = {0};
+    char post[290]            = {0};
 
-  // GET REQUEST
-  // snprintf(post,sizeof(post),"GET /%s HTTP/1.1\r\nHost: %s\r\nContent-Type: application/x-www-form-urlencoded\r\nContent-Length: %d\r\n\r\n%s\r\n\r\n",route,HOST_IP,strlen(mssg),mssg);
+    snprintf(espCommandString, sizeof(espCommandString),"AT+CIPSTART=\"TCP\",\"%s\",%s\r\n",HOST_IP,HOST_PORT); 
+    espSend(espCommandString);    //starts the connection to the server
+    delay(stay);
 
-  // POST REQUEST
-  snprintf(post, sizeof(post), "POST /%s HTTP/1.1\r\nHost: %s\r\nContent-Type: application/json\r\nContent-Length: %d\r\n\r\n%s\r\n\r\n", route, HOST_IP, strlen(mssg), mssg);
+    // GET REQUEST 
+    // snprintf(post,sizeof(post),"GET /%s HTTP/1.1\r\nHost: %s\r\nContent-Type: application/x-www-form-urlencoded\r\nContent-Length: %d\r\n\r\n%s\r\n\r\n",route,HOST_IP,strlen(mssg),mssg);
 
-  snprintf(espCommandString, sizeof(espCommandString), "AT+CIPSEND=%d\r\n", strlen(post));
-  espSend(espCommandString); // sends post length
-  delay(stay);
-  Serial.println(post);
-  espSend(post); // sends POST request with the parameters
-  delay(stay);
-  espSend("AT+CIPCLOSE\r\n"); // closes server connection
-}
+    // POST REQUEST
+    snprintf(post,sizeof(post),"POST /%s HTTP/1.1\r\nHost: %s\r\nContent-Type: application/json\r\nContent-Length: %d\r\n\r\n%s\r\n\r\n",route,HOST_IP,strlen(mssg),mssg);
+  
+    snprintf(espCommandString, sizeof(espCommandString),"AT+CIPSEND=%d\r\n", strlen(post));
+    espSend(espCommandString);    //sends post length
+    delay(stay);
+    Serial.println(post);
+    espSend(post);                //sends POST request with the parameters 
+    delay(stay);
+    espSend("AT+CIPCLOSE\r\n");   //closes server connection
+   }
 
-void espInit()
-{
-  char connection[100] = {0};
-  esp.begin(115200);
-  Serial.println("Initiallizing");
-  esp.println("AT");
-  delay(1000);
-  esp.println("AT+CWMODE=1");
-  delay(1000);
-  while (esp.available())
-  {
-    Serial.println(esp.readString());
-  }
+void espInit(){
+    char connection[100] = {0};
+    esp.begin(115200); 
+    Serial.println("Initiallizing");
+    esp.println("AT"); 
+    delay(1000);
+    esp.println("AT+CWMODE=1");
+    delay(1000);
+    while(esp.available()){ Serial.println(esp.readString());} 
 
-  snprintf(connection, sizeof(connection), "AT+CWJAP=\"%s\",\"%s\"\r\n", SSID, password);
-  esp.print(connection);
+    snprintf(connection, sizeof(connection),"AT+CWJAP=\"%s\",\"%s\"\r\n",SSID,password);
+    esp.print(connection);
 
-  delay(3000); // gives ESP some time to get IP
+    delay(3000);  //gives ESP some time to get IP
 
-  if (esp.available())
-  {
-    Serial.print(esp.readString());
-  }
-
-  Serial.println("\nFinish Initializing");
+    if(esp.available()){   Serial.print(esp.readString());}
+    
+    Serial.println("\nFinish Initializing");    
+   
 }
 
 //***** Design and implement all util functions below ******
-double getWaterHeight(double radar)
+float checkWaterheight()
 {
-  // Calculate water height using the radar measurement
-  double val = radar + 16.737;
-  return SENSOR_HEIGHT - val;
+  digitalWrite(TRIG, LOW);
+  delayMicroseconds(2);
+  digitalWrite(TRIG, HIGH);
+  delayMicroseconds(10);
+  digitalWrite(TRIG, LOW);
+
+    // Read echo duration
+  duration = pulseIn(ECHO, HIGH);
+    
+    // Calculate distance (cm)
+  distance = (duration * 0.034) / 2;
+  return sensorheight - distance;
+}
+float checkReserve()
+{
+  float waterheight = checkWaterheight();
+  return 3.142 * radius * radius * waterheight * 0.004329;
 }
 
-double getWaterReserve(double height)
+float detpercentage()
 {
-  // Calculate water reserve using the radar measurement
-  // Convert height to gallons using the volume formula for a cylinder
-  double volume = M_PI * TANK_RADIUS * TANK_RADIUS * height / 231.0; // 231 cubic inches in a gallon
-
-  return volume;
+  float waterheight = checkWaterheight();
+  return (waterheight / maxheight) * 100;
 }
